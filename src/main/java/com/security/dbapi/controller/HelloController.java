60b5c2f097361aa974b4cd45d6873b50c08f13b2
JMisiorny <mekambe2@gmail.com>
2018-12-06 20:57:54 +0100

    Initial commit

---
+++ src/main/java/com/security/dbapi/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.security.dbapi.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest")
+@RestController
+public class HelloController {
+    @GetMapping("/all")
+    public String helloONe(){
+        return "Hello One";
+    }
+
+    @GetMapping("/security/all")
+    public String helloSecure(){
+        return "Hello Secure";
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping ("/specialAdmin")
+    public String helloByRole(){
+        return "Hello Secure";
+    }
+
+
+
+
+
+
+}
